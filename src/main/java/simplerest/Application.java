789d0ebbadf5c62572cfde3e5c6ec2630aa2813b
maxsv0 <max.svistunov@gmail.com>
2018-06-19 10:26:53 +0200

    Add Etag support

--- src/main/java/simplerest/Application.java
+++ src/main/java/simplerest/Application.java
@@ -1,12 +1,22 @@
 package simplerest;
 
+import javax.servlet.Filter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
+@ComponentScan
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
+  @Bean
+  public Filter filter() {
+    ShallowEtagHeaderFilter filter = new ShallowEtagHeaderFilter();
+    return filter;
+  }
 }
